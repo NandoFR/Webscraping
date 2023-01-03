db152b6bcb2e017afa7b4731a3724a9c0bc803b3
NandoFR <fernandofr077@gmail.com>
2023-01-03 00:30:20 -0300

    chore(api): create data specs list

---
+++ api/domain/data/SpecsList.go
@@ -0,0 +1,12 @@
+package data
+
+var Specs = map[string]map[string]string{
+	"Memory": {
+		"8GB":   "8GB",
+		"16GB":  "16GB",
+		"32GB":  "32GB",
+		"64GB":  "64GB",
+		"128GB": "128GB",
+		"256GB": "256GB",
+	},
+}

5535dc644f20b1a375185560b851584ae35f5f78
NandoFR <fernandofr077@gmail.com>
2023-01-03 00:33:29 -0300

    chore(api): "TranslateWord" method of "Convert" helper now translate a array of words

--- api/domain/helpers/convert/TranslateWord.go
+++ api/domain/helpers/convert/TranslateWord.go
@@ -6,17 +6,19 @@ func (c Convert) TranslateWord(text string) string {
 
 	text = strings.ToLower(text)
 
-	x := func(old string, replace string) {
-		oldLower := strings.ToLower(old)
+	x := func(olds []string, replace string) {
 		replaceLower := strings.ToLower(replace)
-
-		if strings.Contains(text, oldLower) {
-			text = strings.Replace(text, oldLower, replaceLower, -1)
+		for _, v := range olds {
+			oldLower := strings.ToLower(v)
+			if strings.Contains(text, oldLower) {
+				text = strings.Replace(text, oldLower, replaceLower, -1)
+			}
 		}
 	}
 
-	x("computador", "pc")
-	x("?iphone", "iphone")
+	x([]string{"computador"}, "pc")
+	x([]string{"?iphone"}, "iphone")
+	x([]string{"televisao", "televisão"}, "tv")
 
 	return strings.Title(text)
 }
